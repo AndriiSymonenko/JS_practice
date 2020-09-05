d5a22e79fc5a5fce4173dfebf550b948af406b5c
Andrii Symonenko <andrii.symonenko.dev@gmail.com>
2020-09-05 16:56:22 +0300

    first commit

---
+++ site_window/src/js/modules/forms.js
@@ -0,0 +1,57 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся.',
+        failure: 'Ошибка соединения',
+    };
+
+    const postDate = async (url, data) => {
+        document.querySelector('.status').innerHTML = message.loading;
+        let result = await fetch(url, {
+            method: 'POST',
+            body: data,
+        });
+        return await result.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    }
+
+    form.forEach(item => {
+        item.addEventListener('submit', (event) => {
+            event.preventDefault();
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+            postDate('assets/server.php', formData)
+                .then(result => {
+                    console.log(result);
+                    statusMessage.innerHTML = message.success;
+                })
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;

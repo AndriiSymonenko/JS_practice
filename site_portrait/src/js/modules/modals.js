7c14467a2aaf35fb9a53e7714ef8d12fc6b37076
Andrii Symonenko <andrii.symonenko.dev@gmail.com>
2020-09-16 21:23:34 +0300

    fix bug, add new project, modal windows module

---
+++ site_portrait/src/js/modules/modals.js
@@ -0,0 +1,89 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (event) => {
+                if (event.target) {
+                    event.preventDefault();
+                }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`
+
+            });
+        });
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            modal.style.display = 'none';
+
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+        });
+
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal && closeClickOverlay) {
+                modal.style.display = 'none';
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
+
+            }
+        });
+    }
+
+    function shnowModalByTime(selector, time) {
+        setTimeout(() => {
+            let display;
+
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    display = 'block';
+                }
+            });
+
+            if (!display) {
+                document.querySelector(selector).style.display = 'block';
+                document.body.style.overflow = 'hidden';
+            }
+
+        }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+
+    shnowModalByTime('.popup-consultation', 2000);
+};
+
+export default modals;

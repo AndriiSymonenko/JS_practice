98e1bc56ea0c499976d22c1a909e68d970dfbead
AndriiSymonenko <andrii.symonenko.dev@gmail.com>
2020-10-22 18:55:36 +0300

    add video player

---
+++ site_multipage/src/js/modules/playVideo.js
@@ -0,0 +1,53 @@
+export default class VideoPlayer {
+    constructor(triggers, popup) {
+        this.buttons = document.querySelectorAll(triggers);
+        this.popup = document.querySelector(popup);
+        this.close = this.popup.querySelector('.close');
+    }
+
+    bindTriggers() {
+        this.buttons.forEach(item => {
+            item.addEventListener('click', () => {
+
+                if (document.querySelector('iframe#frame')) {
+                    this.popup.style.display = 'flex';
+                } else {
+                    const pathUrl = item.getAttribute('data-url');
+                    this.createPlayer(pathUrl);
+                }
+
+            }); 
+        });
+    }
+
+    bindCloseBtn() {
+        this.close.addEventListener('click', () => {
+            this.popup.style.display = 'none';
+            this.player.stopVideo();
+        });
+    }
+
+
+    createPlayer(url) {
+            this.player = new YT.Player('frame', {
+                height: '100%',
+                width: '100%',
+                videoId: `${url}`,
+            });
+
+            console.log(this.player);
+            this.popup.style.display = 'flex';
+    }
+
+
+    init() {
+        const tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        this.bindTriggers();
+        this.bindCloseBtn();
+    }
+}
